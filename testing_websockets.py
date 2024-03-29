18d6702f6a7aad2250882aec9524326e6ae41224
Arnav495 <arnieincyberland@gmail.com>
2024-03-28 17:29:24 -0700

    misc changes

--- testing_websockets.py
+++ testing_websockets.py
@@ -1,17 +1,22 @@
+#!/usr/bin/env python3
 import websockets
 import asyncio 
+import umsgpack
 
 async def listen():
-    url = "ws://localhost:5800/websocket_data"
+	url = "ws://localhost:5800/websocket_data"
 
-    async with websockets.connect(url) as ws:
-        for i in range(10):
-            msg = await ws.recv()
-            print(msg)
+	async with websockets.connect(url) as ws:
+		for i in range(10):
+			print(f"{'*' * 50} {i} {'*' * 50}")
+			msg = await ws.recv()
+			#print(msg)
+			unpacked = umsgpack.unpackb(msg)
+			print(unpacked)
 
 asyncio.get_event_loop().run_until_complete(listen())
 
 # with connect("ws://localhost:5800/websocket_data") as websocket:
-#     websocket.send("Hi")
-#     message = websocket.recv()
-#     print(f"Recieved: {message}")
+#	 websocket.send("Hi")
+#	 message = websocket.recv()
+#	 print(f"Recieved: {message}")
