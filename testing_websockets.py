7a271574a273c136be0c382fa7768d7924c9ebdc
johaj10 <78065218+johaj10@users.noreply.github.com>
2024-03-29 21:05:03 -0700

    Get useful data

--- testing_websockets.py
+++ testing_websockets.py
@@ -2,21 +2,41 @@
 import websockets
 import asyncio 
 import umsgpack
+import msgpack
+import json
+import time
+
+photon_settings = {}
 
 async def listen():
-	url = "ws://localhost:5800/websocket_data"
+    url = "ws://localhost:5800/websocket_data"
 
-	async with websockets.connect(url) as ws:
-		for i in range(10):
-			print(f"{'*' * 50} {i} {'*' * 50}")
-			msg = await ws.recv()
-			#print(msg)
-			unpacked = umsgpack.unpackb(msg)
-			print(unpacked)
+    async with websockets.connect(url) as ws:
+        global photon_settings
+        
+        for i in range(2):
+            # print(f"{'*' * 50} {i} {'*' * 50}")
+            msg = await ws.recv()
+            # print(msg)
+            
+            unpacked = msgpack.unpackb(msg)
+            # print(unpacked)
+            
+            if i == 1:
+                photon_settings = unpacked
+
+
 
 asyncio.get_event_loop().run_until_complete(listen())
 
-# with connect("ws://localhost:5800/websocket_data") as websocket:
-#	 websocket.send("Hi")
-#	 message = websocket.recv()
-#	 print(f"Recieved: {message}")
+
+
+print("Hostname: " + photon_settings["settings"]["networkSettings"]["hostname"])
+print(f"{'*' * 50}")
+print(photon_settings["cameraSettings"][0]["nickname"])
+print(photon_settings["cameraSettings"][0]["currentPipelineSettings"]["pipelineNickname"])
+print(photon_settings["cameraSettings"][0]["currentPipelineSettings"]["doMultiTarget"])
+print(photon_settings["cameraSettings"][0]["currentPipelineSettings"]["decisionMargin"])
+print(photon_settings["cameraSettings"][0]["currentPipelineSettings"]["cameraAutoExposure"])
+print(photon_settings["cameraSettings"][0]["currentPipelineSettings"]["cameraExposure"])
+print(photon_settings["cameraSettings"][0]["currentPipelineSettings"]["cameraBrightness"])
